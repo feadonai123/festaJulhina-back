15a5e3f410c15f164e96599e2f41b94c826245f9
feadonai123 <felipe.2002@alunos.utfpr.edu.br>
2022-07-31 17:45:14 -0300

    Retornar roles full na rota GET roles

--- src/dbTest/roles/list.js
+++ src/dbTest/roles/list.js
@@ -1,3 +1,4 @@
+const roles = require("../../../database/dbTeste/roles")
 const Roles = require("../../../database/dbTeste/roles")
 
 module.exports = require('express').Router().get(`/roles`, async (req, res) => {
@@ -6,13 +7,22 @@ module.exports = require('express').Router().get(`/roles`, async (req, res) => {
     const 
       all = req.query.all ? req.query.all == "true" : false,
       resultFindAll = await Roles.findAll({ all })
-    
+
     if(!resultFindAll.success) throw ({
       message: resultFindAll.message,
       status: 400
     })
 
-    result = resultFindAll.data
+    const 
+      listIds = resultFindAll.data.map(role => role._id),
+      resultFindFullAll = await Roles.getFullListRoles(listIds)
+    
+    if(!resultFindFullAll.success) throw ({
+      message: resultFindFullAll.message,
+      status: 400
+    })
+
+    result = resultFindFullAll.data
     status = 200
   }catch(e){
     message = e.message
